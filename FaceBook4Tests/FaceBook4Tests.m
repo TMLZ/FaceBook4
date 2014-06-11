a405b0b88b12794b42551a888b44c1a9973ef8dc
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2014-06-10 20:53:45 -0700

    Initial Commit

---
+++ FaceBook4Tests/FaceBook4Tests.m
@@ -0,0 +1,34 @@
+//
+//  FaceBook4Tests.m
+//  FaceBook4Tests
+//
+//  Created by Tyler Miller on 6/10/14.
+//  Copyright (c) 2014 Tyler Miller. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceBook4Tests : XCTestCase
+
+@end
+
+@implementation FaceBook4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
